2e2d8d747905abcc215f27daba8a8135424d4bea
Miko Chu <miko.codecrafters@gmail.com>
2025-07-26 20:24:50 +0800

    add user details mock integration

--- src/app/admin/user/user-details/user-details.ts
+++ src/app/admin/user/user-details/user-details.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../_shared/service/user-service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../../../_shared/model/user';
 
 @Component({
   selector: 'app-user-details',
@@ -6,6 +9,29 @@ import { Component } from '@angular/core';
   templateUrl: './user-details.html',
   styleUrl: './user-details.css'
 })
-export class UserDetails {
+export class UserDetails implements OnInit {
+  isLoading = false
+  id!: number
+  user?: User
 
+  constructor(
+    private readonly userService: UserService,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoading = true
+
+    this.id = this.route.snapshot.params['id']
+    
+    this.userService.getOne(this.id).subscribe({
+      next: (u) => this.user = u,
+      error: (e) => alert(`Something went wrong ${e}`)
+    }).add(() => this.isLoading = false)
+  }
+
+  onUpdate() {
+    this.router.navigate(['/admin/user/update', this.id])
+  }
 }
