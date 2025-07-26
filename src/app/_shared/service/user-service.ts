7a7f0e3d2eeb227efc33d67cd1e58da9078e0316
Miko Chu <miko.codecrafters@gmail.com>
2025-07-26 20:58:35 +0800

    add user list mock integration

--- src/app/_shared/service/user-service.ts
+++ src/app/_shared/service/user-service.ts
@@ -2,23 +2,38 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/user';
 
+const mockUser: () => User = () => ({
+  _id: '1',
+  firstName: 'Juan',
+  middleName: 'Della',
+  lastName: 'Cruz',
+  emailAddress: 'juan@gmail.com',
+  mobileNumber: '09394252236',
+  address: 'blk 2 lot 75',
+  roles: 'dentist',
+})
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  getAll(): Observable<User[]> {
+    return new Observable((s) => {
+      setTimeout(() => {
+        const items = [
+          mockUser()
+        ]
+
+        s.next(items)
+        s.complete()
+      }, 1000);
+    })
+  }
+
   getOne(id: string): Observable<User> {
     return new Observable((s) => {
       setTimeout(() => {
-        const user: User = {
-          _id: '1',
-          firstName: 'Juan',
-          middleName: 'Della',
-          lastName: 'Cruz',
-          emailAddress: 'juan@gmail.com',
-          mobileNumber: '09394252236',
-          address: 'blk 2 lot 75',
-          roles: 'dentist',
-        }
+        const user = mockUser()
 
         s.next(user)
         s.complete()
