b084b1b8f8b7fd9c742c9ef59b0f96301493a7ed
Miko Chu <miko.codecrafters@gmail.com>
2025-07-26 21:05:09 +0800

    add clinic list component mock integration

--- src/app/admin/clinic/clinic-list/clinic-list.ts
+++ src/app/admin/clinic/clinic-list/clinic-list.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Clinic } from '../../../_shared/model/clinic';
+import { ClinicService } from '../../../_shared/service/clinic-service';
 
 @Component({
   selector: 'app-clinic-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './clinic-list.html',
   styleUrl: './clinic-list.css'
 })
 export class ClinicList {
+  isLoading = false
 
+  clinics: Clinic[] = [];
+
+  constructor(
+    private readonly clinicService: ClinicService,
+    private readonly router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoading = true
+
+    this.clinicService.getAll().subscribe({
+      next: (clinics) => this.clinics = clinics,
+      error: (e) => alert(`Something went wrong ${e}`)
+    }).add(() => this.isLoading = false)
+  }
+
+  onDetails(id: string) {
+    this.router.navigate(['/admin/clinic/details', id])
+  }
+
+  onUpdate(id: string) {
+    this.router.navigate(['/admin/clinic/update', id])
+  }
 }
