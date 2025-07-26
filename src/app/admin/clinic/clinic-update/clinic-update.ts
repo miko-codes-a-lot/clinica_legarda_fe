5c737db8b2ea05f44f9b184dcc81728d192ae54d
Miko Chu <miko.codecrafters@gmail.com>
2025-07-26 22:19:11 +0800

    add clinic update mock integration

--- src/app/admin/clinic/clinic-update/clinic-update.ts
+++ src/app/admin/clinic/clinic-update/clinic-update.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { Clinic } from '../../../_shared/model/clinic';
+import { ClinicService } from '../../../_shared/service/clinic-service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClinicForm } from '../clinic-form/clinic-form';
 
 @Component({
   selector: 'app-clinic-update',
-  imports: [],
+  imports: [ClinicForm],
   templateUrl: './clinic-update.html',
   styleUrl: './clinic-update.css'
 })
 export class ClinicUpdate {
+  isLoading = false
+  id!: string
+  clinic!: Clinic
 
+  constructor(
+    private readonly clinicService: ClinicService,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoading = true
+
+    this.id = this.route.snapshot.params['id']
+
+    this.clinicService.getOne(this.id).subscribe({
+      next: (c) => this.clinic = c,
+      error: (e) => alert(`Something went wrong: ${e}`)
+    }).add(() => this.isLoading = false)
+  }
+
+  onSubmit(clinic: Clinic) {
+    this.isLoading = true
+    this.clinicService.update(this.id, clinic).subscribe({
+      next: () => this.router.navigate(['admin/clinic/details', this.id], { replaceUrl: true }),
+      error: (e) => alert(`Something went wrong: ${e}`)
+    }).add(() => this.isLoading = false)
+  }
 }
