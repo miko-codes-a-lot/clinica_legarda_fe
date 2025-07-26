794327efa4dc64b5ac54bdb35e994448575ec433
Miko Chu <miko.codecrafters@gmail.com>
2025-07-26 20:48:19 +0800

    add clinic service

---
+++ src/app/_shared/service/clinic-service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Clinic } from '../model/clinic';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClinicService {
+    getOne(id: string): Observable<Clinic> {
+      return new Observable((s) => {
+        setTimeout(() => {
+          const clinic: Clinic = {
+            _id: '1',
+            name: 'Clinica Legarda Dental Clinic',
+            address: '2275 Legarda st. Sampaloc Manila',
+            mobileNumber: '+639391112236',
+            emailAddress: 'contact@clinicalegarda.com',
+            operatingHours: [
+              {"day": "Monday", "hours": "09:00-18:00"},
+              {"day": "Tuesday", "hours": "09:00-18:00"},
+              {"day": "Wednesday", "hours": "09:00-18:00"},
+              {"day": "Thursday", "hours": "09:00-18:00"},
+              {"day": "Friday", "hours": "09:00-18:00"},
+              {"day": "Saturday", "hours": "10:00-15:00"},
+              {"day": "Sunday", "hours": "Closed"}
+            ]
+          }
+  
+          s.next(clinic)
+          s.complete()
+        }, 1000);
+      })
+    }
+  
+    create(clinic: Clinic): Observable<Clinic> {
+      return new Observable((s) => {
+        setTimeout(() => {
+          clinic._id = '5'
+          s.next(clinic)
+          s.complete()
+        }, 1000);
+      })
+    }
+  
+    update(id: string, clinic: Clinic): Observable<Clinic> {
+      return new Observable((s) => {
+        setTimeout(() => {
+          clinic._id = 'id'
+          s.next(clinic)
+          s.complete()
+        }, 1000);
+      })
+    }
+  
+    delete(): Observable<void> {
+      return new Observable((s) => {
+        setTimeout(() => {
+          s.next()
+          s.complete()
+        }, 1000);
+      })
+    }
+}
