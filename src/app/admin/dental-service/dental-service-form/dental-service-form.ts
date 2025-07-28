088ba4d883fbd18db75f3407fb26a6231deb9f4b
Miko Chu <miko.codecrafters@gmail.com>
2025-07-28 19:09:09 +0800

    add dental service form binding

--- src/app/admin/dental-service/dental-service-form/dental-service-form.ts
+++ src/app/admin/dental-service/dental-service-form/dental-service-form.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DentalService } from '../../../_shared/model/dental-service';
+import { RxDentalServiceForm } from './rx-dental-service-form';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dental-service-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './dental-service-form.html',
   styleUrl: './dental-service-form.css'
 })
 export class DentalServiceForm {
+  @Output() onSubmitEvent = new EventEmitter<DentalService>()
+  @Input() isLoading = false
+  @Input() dentalService!: DentalService
 
+  rxform!: FormGroup<RxDentalServiceForm>
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.rxform = this.fb.nonNullable.group({
+      name: [this.dentalService.name, Validators.required],
+    })
+  }
+
+  onSubmit() {
+    const service: DentalService = {
+      name: this.name.value,
+    }
+
+    this.onSubmitEvent.emit(service)
+  }
+
+  get name() {
+    return this.rxform.controls.name
+  }
 }
---
+++ src/app/admin/dental-service/dental-service-form/rx-dental-service-form.ts
@@ -0,0 +1,5 @@
+import { FormControl } from "@angular/forms";
+
+export interface RxDentalServiceForm {
+  name: FormControl<string>
+}
