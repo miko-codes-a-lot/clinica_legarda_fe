84e8d3c80e4e7ce3f5bb7e956b4f7b9464c4a8d4
Miko Chu <miko.codecrafters@gmail.com>
2025-08-05 22:58:38 +0800

    fix user update clinics list empty

--- src/app/admin/user/user-update/user-update.ts
+++ src/app/admin/user/user-update/user-update.ts
@@ -7,6 +7,8 @@ import { forkJoin } from 'rxjs';
 import { DayService } from '../../../_shared/service/day-service';
 import { Day } from '../../../_shared/model/day';
 import { UserPayload } from '../user-form/user-payload';
+import { Clinic } from '../../../_shared/model/clinic';
+import { ClinicService } from '../../../_shared/service/clinic-service';
 
 @Component({
   selector: 'app-user-update',
@@ -19,8 +21,10 @@ export class UserUpdate implements OnInit {
   id!: string
   user!: User
   days: Day[] = []
+  clinics: Clinic[] = []
 
   constructor(
+    private readonly clinicService: ClinicService,
     private readonly userService: UserService,
     private readonly dayService: DayService,
     private readonly route: ActivatedRoute,
@@ -33,10 +37,12 @@ export class UserUpdate implements OnInit {
     this.id = this.route.snapshot.params['id']
 
     forkJoin({
+      clinics: this.clinicService.getAll(),
       user: this.userService.getOne(this.id),
       days: this.dayService.getAll(),
     }).subscribe({
-      next: ({ user, days }) => {
+      next: ({ clinics, user, days }) => {
+        this.clinics = clinics
         this.user = user
         this.days = days
       },
