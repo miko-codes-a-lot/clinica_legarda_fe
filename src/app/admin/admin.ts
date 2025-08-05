e0a6f1cbc5c20627d36f0d1ede1b6ac1edffad58
Benj <devbenjpena@gmail.com>
2025-08-05 22:47:56 +0800

    added navigation

--- src/app/admin/admin.ts
+++ src/app/admin/admin.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-admin',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatSidenavModule, MatListModule, MatToolbarModule, MatIconModule],
   templateUrl: './admin.html',
   styleUrl: './admin.css'
 })
