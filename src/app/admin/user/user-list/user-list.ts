0fbcaf54126fd69a9c12c89989f113c20b72aa7b
Benj <devbenjpena@gmail.com>
2025-08-07 01:49:50 +0800

    user table improved - table is to be converted into reusable component

--- src/app/admin/user/user-list/user-list.ts
+++ src/app/admin/user/user-list/user-list.ts
@@ -2,10 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../../_shared/service/user-service';
 import { User } from '../../../_shared/model/user';
+import {MatTableModule} from '@angular/material/table';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 
 @Component({
   selector: 'app-user-list',
-  imports: [RouterLink],
+  imports: [RouterLink, MatTableModule, MatProgressSpinnerModule],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css'
 })
@@ -13,6 +16,7 @@ export class UserList implements OnInit {
   isLoading = false
 
   users: User[] = [];
+  displayedColumns: string[] = ['_id', 'name', 'actions'];
 
   constructor(
     private readonly userService: UserService,
@@ -26,6 +30,8 @@ export class UserList implements OnInit {
       next: (users) => this.users = users,
       error: (e) => alert(`Something went wrong ${e}`)
     }).add(() => this.isLoading = false)
+
+    console.log('users', this.users);
   }
 
   onDetails(id: string) {
