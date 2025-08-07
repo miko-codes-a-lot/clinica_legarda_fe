cb28cf99708e065a51b1d6496a35e40f991e8d1a
Miko Chu <miko.codecrafters@gmail.com>
2025-08-08 07:24:06 +0800

    modify appointment form to use date picker aware dentist schedule & dentist list based on chosen clinic

--- src/app/admin/appointment/appointment-create/appointment-create.ts
+++ src/app/admin/appointment/appointment-create/appointment-create.ts
@@ -3,12 +3,14 @@ import { Appointment } from '../../../_shared/model/appointment';
 import { User } from '../../../_shared/model/user';
 import { UserService } from '../../../_shared/service/user-service';
 import { AppointmentService } from '../../../_shared/service/appointment-service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { forkJoin } from 'rxjs';
 import { AppointmentPayload } from '../appointment-payload';
 import { AppointmentForm } from '../appointment-form/appointment-form';
 import { DentalService } from '../../../_shared/model/dental-service';
 import { DentalServicesService } from '../../../_shared/service/dental-services-service';
+import { ClinicService } from '../../../_shared/service/clinic-service';
+import { Clinic } from '../../../_shared/model/clinic';
 
 @Component({
   selector: 'app-appointment-create',
@@ -19,15 +21,17 @@ import { DentalServicesService } from '../../../_shared/service/dental-services-
 export class AppointmentCreate {
   isLoading = false
   initDoc!: Appointment
+
+  dentalServices: DentalService[] = []
+  clinics: Clinic[] = []
   patients: User[] = []
   dentists: User[] = []
-  dentalServices: DentalService[] = []
 
   constructor(
-    private readonly appointmentService: AppointmentService,
     private readonly dentalServicesService: DentalServicesService,
+    private readonly appointmentService: AppointmentService,
+    private readonly clinicService: ClinicService,
     private readonly userService: UserService,
-    private readonly route: ActivatedRoute,
     private readonly router: Router,
   ) {}
 
@@ -37,14 +41,14 @@ export class AppointmentCreate {
     this.initDoc = this.appointmentService.getEmptyNonNullDoc()
 
     forkJoin({
-      patients: this.userService.getAll(),
-      dentists: this.userService.getAll(),
       services: this.dentalServicesService.getAll(),
+      clinics: this.clinicService.getAll(),
+      patients: this.userService.getAll(),
     }).subscribe({
-      next: ({ patients, dentists, services }) => {
-        this.patients = patients
-        this.dentists = dentists
+      next: ({ services, clinics, patients }) => {
         this.dentalServices = services
+        this.clinics = clinics
+        this.patients = patients
       },
       error: (e) => alert(`Something went wrong ${e}`),
       complete: () => this.isLoading = false,
--- src/app/admin/appointment/appointment-form/appointment-form.ts
+++ src/app/admin/appointment/appointment-form/appointment-form.ts
@@ -6,31 +6,76 @@ import { User } from '../../../_shared/model/user';
 import { RxAppointmentForm } from './rx-appointment-form';
 import { DentalService } from '../../../_shared/model/dental-service';
 import { FormControlErrorsComponent } from '../../../_shared/component/form-control-errors/form-control-errors.component';
+import { Clinic } from '../../../_shared/model/clinic';
+import { DatePicker } from '../../../_shared/component/date-picker/date-picker';
 
 @Component({
   selector: 'app-appointment-form',
-  imports: [ReactiveFormsModule, FormControlErrorsComponent],
+  imports: [
+    ReactiveFormsModule,
+    FormControlErrorsComponent,
+    DatePicker,
+  ],
   templateUrl: './appointment-form.html',
   styleUrl: './appointment-form.css'
 })
 export class AppointmentForm {
   @Output() onSubmitEvent = new EventEmitter<AppointmentPayload>()
   @Input() isLoading = false
+  @Input() clinics: Clinic[] = []
   @Input() appointment!: Appointment
   @Input() dentalServices: DentalService[] = []
   @Input() patients: User[] = []
-  @Input() dentists: User[] = []
+
+  dentists: User[] = []
+  selectedDentist?: User
 
   rxform!: FormGroup<RxAppointmentForm>
 
+  minDate = new Date()
+  maxDate = new Date(Date.now() + 90 * 24 * 60 * 60 * 1000) // 90 days from now only
+
   constructor(private readonly fb: FormBuilder) {}
 
+  private changeDentists(clinicId: string) {
+    const clinic = this.clinics.find(c => c._id == clinicId)
+    if (!clinic) return
+
+    this.dentists = clinic.dentists
+  }
+
+  private setDentist(dentistId: string) {
+    const dentist = this.dentists.find(d => d._id == dentistId)
+    if (!dentist) return
+
+    this.selectedDentist = dentist
+  }
+
   ngOnInit(): void {
+    const clinicId = this.appointment.clinic._id || ''
+    const dentistId = this.appointment.dentist._id || ''
+
     this.rxform = this.fb.nonNullable.group({
-      dentist: [this.appointment.dentist._id || '', Validators.required],
+      clinic: [clinicId, Validators.required],
+      dentist: [dentistId, Validators.required],
       patient: [this.appointment.patient._id || '', Validators.required],
       services: [this.appointment.services.map(s => s._id || ''), Validators.required],
-      date: [this.appointment.date.toISOString(), Validators.required]
+      date: [this.appointment.date, Validators.required]
+    })
+
+    this.changeDentists(clinicId)
+    this.setDentist(dentistId)
+
+    this.clinic.valueChanges.subscribe({
+      next: (v) => {
+        this.changeDentists(v)
+      }
+    })
+
+    this.dentist.valueChanges.subscribe({
+      next: (d) => {
+        this.setDentist(d)
+      }
     })
   }
 
@@ -39,7 +84,7 @@ export class AppointmentForm {
       dentist: this.dentist.value,
       patient: this.patient.value,
       services: this.services.value,
-      date: new Date(this.date.value),
+      date: this.date.value,
       status: AppointmentStatus.PENDING,
       notes: {
         clinicNotes: '',
@@ -49,6 +94,10 @@ export class AppointmentForm {
     }
 
     this.onSubmitEvent.emit(appointment)
+  }
+
+  get clinic() {
+    return this.rxform.controls.clinic
   }
 
   get dentist() {
