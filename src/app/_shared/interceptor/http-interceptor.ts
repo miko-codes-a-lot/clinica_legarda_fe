77e04a02556aae852dbb49e25954acaf740f25c9
Miko Chu <miko.codecrafters@gmail.com>
2025-10-10 19:52:15 +0800

    add base url for api calls and with secure enable

---
+++ src/app/_shared/interceptor/http-interceptor.ts
@@ -0,0 +1,10 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+export const httpInterceptor: HttpInterceptorFn = (req, next) => {
+  const reqWithCreds = req.clone({
+    url: `${environment.apiUrl}${req.url}`,
+    withCredentials: true,
+  });
+  return next(reqWithCreds);
+};
